31e7f10778977f963b986109a7c78698dfb9ffcd
Nefilopez <nlopez78@asu.edu>
2024-10-01 14:41:31 -0700

    updated

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-document.getElementById("firstPara").innerHTML += "<span class = 'tan'>This is neewe tan text.</span>";
+//selecting by id
+document.getElementById("firstPara").innerHTML += "<span class = 'tan'>This is new tan text.</span>";
+
+//seleecting by tag name 
+let images = document.getElementsByTagName("img");
+for(let image of images){
+    image.width = "250";
+}
+
+//slecting by class name 
+let ltBlueSpans = document.getElementsByClassName("light-blue");
+let medBlueSpans = document.getElementsByClassName("med-blue");
+console.log(ltBlueSpans);
+
+for(let span of ltBlueSpans){
+    span.style.color = "#7C9EA6";
+}
+for(let span of medBlueSpans){
+    span.style.color = "#3C5E73";
+}
+
+//select many with css selector
+// array of colors
+
+let colors = ["#283040", "#3C5E73", "#7C9EA6", "#D9BCA3", "F2DCC9"];
+
+let svgs = document.querySelectorAll("#svgs svg");
+
+for(let i = 0; i < 5; i++){
+    svgs[i].style.stroke = colors[i];
+}
+
